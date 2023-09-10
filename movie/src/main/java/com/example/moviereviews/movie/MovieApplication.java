e72a4840d8e2a874f5d098f3d32dafbfe4210ee4
hhoque1790 <hhoque500@gmail.com>
2023-09-10 21:33:42 +0100

    Connecting SpringBoot to MongoDB

--- movie/src/main/java/com/example/moviereviews/movie/MovieApplication.java
+++ movie/src/main/java/com/example/moviereviews/movie/MovieApplication.java
@@ -3,21 +3,11 @@ package com.example.moviereviews.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
-@RestController
 
 public class MovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello World!";
-	}
-
 }
